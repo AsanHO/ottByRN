4d62fec3f0d3cdda22b496bf5bf62138d9cd9426
AsanHO <gorma00@naver.com>
2022-07-19 11:18:46 +0900

    preload

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Text, View } from "react-native";
+import AppLoading from "expo-app-loading";
+import React, { useState } from "react";
+import { Text, Image } from "react-native";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset, useAssets } from "expo-asset";
 
 export default function App() {
-  return null;
+  const [assets] = useAssets([require("./preview.png")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+  if (!assets || !loaded) {
+    return <AppLoading />;
+  }
+  return <Text>We are done loading!</Text>;
 }
